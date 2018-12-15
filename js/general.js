31da57b544439ff1f31d3b63d3819085b84b0947
nanfuka <kalungi2k6@gmail.com>
2018-12-15 04:39:18 +0300

    Add js for user_dash

--- js/general.js
+++ js/general.js
@@ -9,3 +9,44 @@ function Signup(){
     document.getElementById("messag").style.display = "none"
     document.getElementById("login").style.display = "none"
 }
+function edit_intervention(){
+    var edit = prompt ("please edit your intervation")
+    if (edit != null){
+        document.getElementById("intervantion").innerHTML= edit
+    }
+}
+function delete_intervantion(){
+    if (confirm("do you really want to delete this intervantion record")){
+        var element =document.getElementById("intervantion");
+        element.parentNode.removeChild(element)
+    }
+
+
+}
+
+// function deletes_intervantion(){
+//     if (confirm("do you really want to delete this intervantion record")){
+//         var element =document.getElementById("intervantions");
+//         element.parentNode.removeChild(element)
+//     }
+
+
+// }
+// function edits_intervention(){
+//     var edit = prompt ("please edit your intervation")
+//     if (edit != null){
+//         document.getElementById("intervantions").innerHTML= edit
+//     }
+// }
+ function Bribe(){
+    document.getElementById("bribery").style.display = "block"
+    document.getElementById("embezzler").style.display = "none"
+
+ }
+ function Embezzle(){
+    document.getElementById("embezzler").style.display = "block"
+//     document.getElementById("bribery").style.display = "none"
+  
+
+ }
+
