2cca48386dcc22941477ad0e3de2dc06878d3364
nanfuka <kalungi2k6@gmail.com>
2018-12-18 15:59:18 +0300

    Refactor code

--- js/general.js
+++ js/general.js
@@ -1,11 +1,20 @@
 var login = document.getElementById("login")
 function Login(){
-    document.getElementById("login").style.display = "block"
-    document.getElementById("messag").style.display = "none"
+    document.getElementById("signin").style.display = "block"
     document.getElementById("signup").style.display = "none"
-    document.getElementById("admin-login").style.display = "none"
+    document.getElementById("signinadmin").style.display = "none"
+    // document.getElementById("signup").style.display = "none"
+    // document.getElementById("admin-login").style.display = "none"
 }
 
+function Viewreports(){
+    document.getElementById("corruption-cases").style.display = "block"
+    document.getElementById("reportform").style.display = "none" 
+}
+function Makereport(){
+    document.getElementById("reportform").style.display = "block"
+    document.getElementById("corruption-cases").style.display = "none" 
+}
 function AdminLogin(){
     document.getElementById("login").style.display = "none"
     document.getElementById("messag").style.display = "none"
@@ -13,35 +22,48 @@ function AdminLogin(){
     document.getElementById("admin-login").style.display = "block"   
 }
 function Signup(){
+    document.getElementById("signin").style.display = "none"
     document.getElementById("signup").style.display = "block"
-    document.getElementById("messag").style.display = "none"
-    document.getElementById("login").style.display = "none"
-    document.getElementById("admin-login").style.display = "none"
+    // document.getElementById("signin").style.display = "block"
+
     
 }
 function edit_intervention(){
-    var edit = prompt ("please edit your intervation")
-    if (edit != null){
-        document.getElementById("intervantion").innerHTML= edit
+    document.getElementById("editintervation").style.display = "block"
+    document.getElementById("submit").style.display = "block"
+    var edit = document.getElementById("editintervation").innerHTML
+    document.getElementById("intervantion").innerHTML= edit
+    // var edit = prompt ("please edit your intervation")
+    // if (edit != null){
+
     }
+
+function edit_geographical(){
+    document.getElementById("editlocation").style.display = "block"
+    document.getElementById("submits").style.display = "block"
+    var edit = document.getElementById("editintervation").innerHTML
+    document.getElementById("intervantion").innerHTML= edit   
 }
+
 function delete_intervantion(){
-    if (confirm("do you really want to delete this intervantion record")){
-        var element =document.getElementById("intervantion");
-        element.parentNode.removeChild(element)
-    }
-
+    // if (confirm("do you really want to delete this intervantion record")){
+    //     var element =document.getElementById("intervantion");
+    //     element.parentNode.removeChild(element)
+    document.getElementById("intervantion").style.display = "none"  
 
 }
+
+
+
 
  function Bribe(){    
-    document.getElementById("embezzmler").style.display = "none";
-    // document.getElementById("embezkmler").style.display = "none" 
+    document.getElementById("bribedetail").style.display = "block";
+    document.getElementById("embezzlement").style.display = "none" 
     // document.getElementsById("corruption-cases").style.display = "none"
  }
  function Embezzle(){
-    document.getElementById("bribnery").style.display = "none"
-    document.getElementById("embezkmler").style.display = "block"
+    document.getElementById("bribedetail").style.display = "none"
+    document.getElementById("embezzlement").style.display = "block"
     // document.getElementById("extortion").style.display = "none" 
   
 
